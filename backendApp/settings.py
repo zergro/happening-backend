51462c0385aaaccb064e5284211bf107810f43f9
zergro <erling_95@hotmail.com>
2022-06-30 15:52:42 +0300

    forgot to change debug to false for production

--- backendApp/settings.py
+++ backendApp/settings.py
@@ -22,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 env = environ.Env(
+    DEBUG=(bool, False)
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
